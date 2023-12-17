8257c7e3dbc7dbdcd40d59f6230fd349419671ac
adry95 <143231440+adry95@users.noreply.github.com>
2023-12-17 20:02:58 +0000

    1

--- script.js
+++ script.js
@@ -3,9 +3,10 @@
 var audio_confirm = new Audio("sound/snd_select.wav");
 var audio_select = new Audio("sound/snd_movemenu.wav");
 var audio_music = new Audio("sound/donde_estan.ogg");
-var audio_music_is_playing = true;
 audio_music.play();
-audio_music.volume = 0.2;
+var audio_music_is_playing = true;
+
+audio_music.volume = 0; //default = 0.2
 audio_music.loop = true;
 
 // Funciones para cambiar el boton de descarga
@@ -45,24 +46,46 @@ function offHover() {
   // Declaracion de variables para detectar la hora actual
   var hoy = new Date();
   var fecha = hoy.getDate()+'-'+(hoy.getMonth()+1);
+  var edad = hoy.getFullYear() - 1977;
 
   // If statement para mostrar el texto de DY
 if (fecha == "31-10") { // Si es Hallowenn
-    speach = "Feliz Halloween mi gente. \n Soy Daddy Yankee";
+    speach = "Feliz Halloween mi gente. <br> Soy Daddy Yankee";
 } else if (fecha == "25-12" || fecha == "24-12") { // Si es Navidad o Nochebuena
-    speach = "Feliz Navidad. \n Y Feliz Año Nuevo. \n ¿Qué? ¿qué? ¡Toma mango!"
+    speach = "Feliz Navidad. <br> Y Feliz Año Nuevo. <br> ¿Qué? ¿qué? ¡Toma mango!";
+} else if (fecha == "3-2") { // Si es su cumpleaños
+    speach = "Feliz cumpleaños a mí, Daddy Panties. ¡El legendario! ¡" + edad + " años! ¡Me duele la cadera!";
 } else { // Elegir aleatoriamente una frase del array para mostrar
     var speach_array = []; // Declaracion de los textos a elegir
-    speach_array[0] = "HEY EVERYBODY!\nDaddy Pantis al habla. Quien no se ha escondido, tiempo a tenido.";
-    speach_array[1] = "¿Dónde están las gatas que no hablan y tiran pa'lante?";
-    speach_array[2] = "MA- MA- MASIVO!\nMA- MA- MASIVO!\nMA- MA- MASIVO!";
+    speach_array[0] = "¡DY! Quien no se ha escondido, tiempo a tenido.";
+    speach_array[1] = "¿Dónde están las gatas?";
+    speach_array[2] = "MA- MA- MASIVO!<br>MA- MA- MASIVO!<br>MA- MA- MASIVO!";
+    speach_array[3] = "¡¡RUMBA!!<br> ¿Quién dejó la ropa sin planchar?";
+    speach_array[4] = "¡Que levante la mano todo el mundo que se puso deshodorante!";
+    speach_array[5] = "Hoy yo salgo pa' la calle sin rumbo, sin GPS. ¡Me quedé sin batería! ¿Quién cargador de iPhone?";
+    speach_array[6] = "El patio de mi casa es particular.<br>Llueve y no se moja, como los demás.";
+    speach_array[7] = "¡Cada segundo, cada minuto que pase sin descargar el juego es tiempo perdido!";
+    speach_array[8] = "¡Legendaddy! ¡Di di diwá!<br>¿Quién coño es Gabi music?";
+    speach_array[9] = "¡Llamen al 911!<br>¡La abuela se cayó por las escaleras!";
+    speach_array[10] = "Escribir diálogos es duro, mis reyes. ¡Me duele el culo! ¡30 cm de regla!";
+    speach_array[11] = "¡Dale, dale don, dale!<br>¡Pa que se muev-!<br>Mierda, esa letra no es mía.";
+    speach_array[12] = "¡El Cangri!<br>¡Se me escapó el tranvía!<br>¡Lo que pasó, pasó!";
+    speach_array[13] = "¡No corras para coger el tranvía! ¡Pasa otro dentro de 3 min!";
+    speach_array[14] = "¡Da-ddy! ¡Yan-keee!<br>¡Hasta luego mi gente, se me escapa el tranvía!";
+    speach_array[15] = "¡Hablé con el hombre que vi en el espejo! ¡Estoy en el psiquiatra!";
+    speach_array[16] = "Recuerda:<br>Tú eres hielo.<br>Y yo soy fuego.";
+    speach_array[17] = "¡La temperatura sube!<br>¡Que la ULL compre ventiladores!";
+    speach_array[18] = "¡Muerte por love and rumba!";
+    speach_array[19] = "El talento de barrio.<br>Psicólogo. Psicólogo.<br>Psiqui-daddy yankee yo.";
+    speach_array[20] = "¡The Big Boss! ¡DY!<br>¿Quién ha dejado la mierda en el baño? ¡Huele que apesta!";
+    speach_array[21] = "¡Mr. Generaciones! ¡Yo me caí y luego me levanté! ¡Me duelen las rodillas!";
+    speach_array[22] = "¡La cabra! ¡Díselo, ma'!<br>¡Castigado sin Play!";
+    //speach_array[] = "";
     speach = speach_array[Math.floor(Math.random() * speach_array.length)]; // Elegir random un index de array
+    //speach = speach_array[speach_array.length-1];
 }
-speach = speach.replace('\n' , '<br>'); // Primer reemplazo
-speach = speach.replace('\n' , '<br>'); // Segundo reemplazo
 
   function speech(){
     document.write(speach);
   }
-
 
