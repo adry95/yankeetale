9c4e68d3af52b163a34b8afcd69f3e9d30023140
adry95 <143231440+adry95@users.noreply.github.com>
2024-01-19 23:00:53 +0000

    Update script.js

--- script.js
+++ script.js
@@ -56,31 +56,31 @@ function offHover() {
 // Frases a mostrar
 var speach_array = []; // Declaracion del array
 speach_array[0] = "¡D.Y.!<br>Quien no se ha escondido, tiempo a tenido.";
-speach_array[1] = "Mi contraseña de Messenger es Broly Superstar.<br>¡El legendario!";
+speach_array[1] = "Mi contraseña de Messenger es Broly Superstar. ¡El legendario!";
 speach_array[2] = "MA- MA- MASIVO!<br>MA- MA- MASIVO!<br>MA- MA- MASIVO!";
-speach_array[3] = "¡¡RUMBA!!<br> ¿Quién dejó la ropa sin planchar?";
+speach_array[3] = "¡¡RUMBA!!<br> ¿Quién dejó la ropa sin planchar?<br>¡Que mañana tengo comunión!";
 speach_array[4] = "¡Que levante la mano todo el mundo que se puso deshodorante!";
 speach_array[5] = "Hoy yo salgo pa' la calle sin rumbo, sin GPS. ¡Me quedé sin batería!";
-speach_array[6] = "El patio de mi casa es particular.<br>Llueve y sí se moja, como los demás.";
+speach_array[6] = "El patio de mi casa es particular.<br>Llueve y no se moja, como los demás.";
 speach_array[7] = "¡Cada segundo, cada minuto que pase sin descargar el juego es tiempo perdido!";
 speach_array[8] = "¡Legendaddy! ¡Di di diwá!<br>¿Quién coño es Gabi music?";
 speach_array[9] = "¡Llamen al 911!<br>¡La abuela se cayó por las escaleras!";
-speach_array[10] = "Escribir diálogos es duro, mis reyes. ¡Me duele el culo! ¡30 cm de regla!";
+speach_array[10] = "Aquí D.Y. con el Yankkeconsejo<br>del día: \"Si el mojón no baja,<br>¡tira de la cisterna!\"";
 speach_array[11] = "¡Dale, dale don, dale!<br>¡Pa que se muev-! ...<br>Espera, esa letra no es mía.";
 speach_array[12] = "¡El Cangri!<br>¡Se me escapó el tranvía!<br>¡Lo que pasó, pasó!";
-speach_array[13] = "¡No corras para coger el tranvía! ¡Pasa otro dentro de 3 min!";
-speach_array[14] = "¡Da-ddy! ¡Yan-keee!<br>¡Hasta luego mi gente, se me escapa el tranvía!";
-speach_array[15] = "¡Hablé con el hombre que vi en el espejo! ¡Estoy en el psiquiatra!";
-speach_array[16] = "Tú eres hielo.<br>Y yo soy fuego.";
-speach_array[17] = "¡La temperatura sube!<br>¡Que la ULL compre ventiladores!";
-speach_array[18] = "¡Muerte por love and rumba!";
+speach_array[13] = "Yankeeconsejo del día:<br>¡No corras para coger el tranvía! ¡Pasa otro en 3 minutos!";
+speach_array[14] = "¡Da-ddy! ¡Yan-keee!<br>¡Hasta luego mi gente, que se me escapa el tranvía!";
+speach_array[15] = "Yankeeconsejo del día:<br>Hablen con el hombre que ven en el espejo. ¡Nunca les dará la espalda!";
+speach_array[16] = "Tú eres hielo.<br>Y yo soy fuego.<br>¡Se me queman las cachapas!";
+speach_array[17] = "¡La temperatura sube!<br>¡Y la ULL no compra ventiladores!";
+speach_array[18] = "¡Muerte por amor y rumba!";
 speach_array[19] = "El talento de mi barrio.<br>Musicólogo.<br>Musicólogo.";
-speach_array[20] = "¡The Big Boss! ¡DY!<br>¿Quién ha dejado la mierda en el baño? ¡Huele que apesta!";
-speach_array[21] = "¡Mr. Generaciones! ¡Yo me caí y luego me levanté! ¡Me duelen las rodillas!";
+speach_array[20] = "¡The Big Boss! ¡D.Y.!<br>¿Quién ha dejado la mierda en el baño? ¡Huele que apesta!";
+speach_array[21] = "¡Mr. Generaciones!<br>¡Yo me caí y luego me levanté!<br>¡La abuela no!";
 speach_array[22] = "¡La cabra! ¡Díselo, ma'!<br>¡Castigado sin Play!";
-speach_array[22] = "¡Llamen a Oppenheimer!<br>¡Soltaron una bomba en el baño!";
-speach_array[23] = "¡Cada una de arroz, son dos de agua!";
-speach_array[24] = "¡Kevin cruzó y le atropelló<br>un Twingo!<br>¡Ajo y agua!";
+speach_array[23] = "¡Llamen a Oppenheimer!<br>¡Soltaron una bomba en el baño!";
+speach_array[24] = "Yankeeconsejo del día:<br>¡Cada una de arroz, son dos de agua!";
+speach_array[25] = "¡Kevin cruzó y le atropelló<br>un Twingo!<br>¡Ajo y agua!";
 //speach_array[] = "";  <-- esto nada mas es pa hacer copy-paste si me hace falta añadir más frases
 
 
@@ -102,7 +102,7 @@ function refresh() {
     if (!last_speech) {
         last_speech = {
             frase: "Daddy Yankee al habla.<br>Bienvenido a la página oficial de Yankeetale. ¡Gózate el juego, rey!",
-            fecha_ultima: midnight //midnight.toJSON()
+            fecha_ultima: midnight
         };
         
     } // Si existe mensaje anterior pero es de ayer o es una fecha especial, elegir nueva frase
@@ -118,7 +118,7 @@ function refresh() {
             fecha_ultima: midnight
         };
     }
-    else if (fecha == "1-1") { // Si es Año Nuevo
+    else if (fecha == "31-12" || fecha == "1-1") { // Si es Nochevieja o Año Nuevo
         last_speech = {
             frase: "Y Victor le dice a Joel<br><br> Feliz Año Nuevo.",
             fecha_ultima: midnight
@@ -132,7 +132,7 @@ function refresh() {
     }
     else if (fecha == "23-5") { // Si es mi cumple
         last_speech = {
-            frase: "¡El marico que hizo esto cumple años! ¡Ya te queda menos pa' probar ataúd! ¡Súbele ritmo!",
+            frase: "¡El marico que hizo esto cumple años! ¡Ya te queda menos pa' probar ataúd! ¡Súbele mambo!",
             fecha_ultima: midnight
         };
     }
@@ -153,11 +153,10 @@ function refresh() {
     console.log('fecha_ultima: '+last_speech.fecha_ultima);
     console.log('midnight: '+midnight);
     console.log('fecha: '+fecha);
-    
 }
 
 refresh();
-
+//speach = speach_array[26];
 function speech(){
     document.write(speach);
 }
